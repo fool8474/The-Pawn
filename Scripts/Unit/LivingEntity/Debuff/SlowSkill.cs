0fe792ba3c4f17e08015d0ff85f3818602fe811d
fool8474 <fool8474@naver.com>
2021-02-08 08:41:02 +0900

    Upload Scripts

---
+++ Scripts/Unit/LivingEntity/Debuff/SlowSkill.cs
@@ -0,0 +1,45 @@
+﻿////////////////////////////////////////////////////
+/*
+    File SlowSkill.cs
+    class SlowSkill
+    
+    담당자 : 안영훈
+    부 담당자 : 
+
+    슬로우 효과를 주는 디버퍼 (미완성)
+    Player 스텟 관련 사항이 너무 복잡하여 아직 적용 시키지 못하였음 
+*/
+////////////////////////////////////////////////////
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowSkill : Debuffer
+{
+    [SerializeField] float slowingFactor;
+    public override void ApplyDebuff(GameObject unit)
+    {
+        float roll = Random.Range(0, 100);
+        if (roll <= Proc)
+        {
+            unit.GetComponent<LivingEntity>().DebuffManager.AddDebuff(GetDebuff(unit.GetComponent<LivingEntity>()));
+        }
+    }
+
+    public override Debuff GetDebuff(LivingEntity unit)
+    {
+        //웨폰마스터리에서 디버프 능력치 구하기?
+        return new SlowDebuff(slowingFactor, DebuffDuration, unit);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+            if (other.CompareTag("Player"))
+            {
+                ApplyDebuff(other.gameObject);
+            }
+        
+
+    }
+}
---
+++ Scripts/Unit/LivingEntity/Monster/MonsterData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CSVReader.Data("id")]
+public class MonsterData
+{
+    [Header("몬스터 기본 정보")]
+    public int id;
+    public string name;
+    public float hp;
+    public float findrange;
+    public float attackrange;
+    public float LimitTraceRange;
+    public float AttackSpeed;
+    public int damage;
+    public float speed;
+    public int gold;
+    public int coin;
+    public int xp;
+}
